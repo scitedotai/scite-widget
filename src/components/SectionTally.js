9a8cd774b659763eef026903d958265da701b394
ashish <ashish@ashishs-MacBook-Pro.local>
2022-04-19 14:19:52 -0500

    [scitedotai/scite#4290] Restyle to allow for optional graphic on left

--- src/components/SectionTally.js
+++ src/components/SectionTally.js
@@ -3,12 +3,13 @@ import qs from 'query-string'
 import classNames from 'classnames'
 import SectionTallyCount from './SectionTallyCount'
 
-import styles from '../styles/Tally.css'
+import styles from '../styles/SectionTally.css'
 
 const SectionTally = ({
   source, campaign, autologin, rewardfulID,
   tally, forceCollapse, showLabels,
-  small = false, horizontal = false, isBadge = false, showZero = true
+  small = false, horizontal = false, isBadge = false, showZero = true,
+  showBarChart = true
 }) => {
   const params = {
     utm_medium: isBadge ? 'badge' : 'plugin',
@@ -42,28 +43,43 @@ const SectionTally = ({
   const discussion = (tally && tally.discussion && tally.discussion.toLocaleString()) || 0
   const other = (tally && tally.other && tally.other.toLocaleString()) || 0
 
+  const reportLink = `https://scite.ai/reports/${tally && tally.doi}?${queryString}`
   const handleClick = () => {
-    window.open(`https://scite.ai/reports/${tally && tally.doi}?${queryString}`)
+    window.open(reportLink)
   }
 
   return (
     <div
-      className={classes.sectionTally}
-      onClick={handleClick}
+      className={styles.sectionTallyWidget}
     >
-      {!horizontal && (
-        <img
-          className={classNames(styles.logo, {
-            [styles.logoSmall]: small
-          })}
-          src='https://cdn.scite.ai/assets/images/logo.svg'
-        />
-      )}
-      <SectionTallyCount type='introduction' count={introduction} horizontal={horizontal} showLabels={showLabels} small={small} />
-      <SectionTallyCount type='methods' count={methods} horizontal={horizontal} showLabels={showLabels} small={small} />
-      <SectionTallyCount type='results' count={results} horizontal={horizontal} showLabels={showLabels} small={small} />
-      <SectionTallyCount type='discussion' count={discussion} horizontal={horizontal} showLabels={showLabels} small={small} />
-      <SectionTallyCount type='other' count={other} horizontal={horizontal} showLabels={showLabels} small={small} />
+        {!horizontal && (
+          <img
+            className={classNames(styles.logo, {
+              [styles.logoSmall]: small
+            })}
+            src='https://cdn.scite.ai/assets/images/logo.svg'
+          />
+        )}
+        <div
+          onClick={handleClick}
+          className={styles.sectionTallyWrapper}
+        >
+          {showBarChart && (
+            <div>
+              <img src='https://chartio.com/assets/24e451/tutorials/charts/grouped-bar-charts/c1fde6017511bbef7ba9bb245a113c07f8ff32173a7c0d742a4e1eac1930a3c5/grouped-bar-example-1.png' />
+              <a href={reportLink} target='_blank'>View more details</a>
+            </div>
+          )}
+          <div
+            className={classes.sectionTally}
+          >
+            <SectionTallyCount type='introduction' count={introduction} horizontal={horizontal} showLabels={showLabels} small={small} />
+            <SectionTallyCount type='methods' count={methods} horizontal={horizontal} showLabels={showLabels} small={small} />
+            <SectionTallyCount type='results' count={results} horizontal={horizontal} showLabels={showLabels} small={small} />
+            <SectionTallyCount type='discussion' count={discussion} horizontal={horizontal} showLabels={showLabels} small={small} />
+            <SectionTallyCount type='other' count={other} horizontal={horizontal} showLabels={showLabels} small={small} />
+          </div>
+        </div>
     </div>
   )
 }
