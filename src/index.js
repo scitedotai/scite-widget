559666854cdbded2e9f4b8dafeb58f5a4560a88f
ashish <ashish@ashishs-MacBook-Pro.local>
2022-04-26 09:33:01 -0500

    [scitedotai/scite#4290] Expose SectionTallyCount

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import HideableTally from './components/HideableTally'
 import Count from './components/Count'
 import TallyLoader from './components/TallyLoader'
 import SectionTally from './components/SectionTally'
+import SectionTallyCount from './components/SectionTallyCount'
 
-export { HideableTally, Tally, Count, TallyLoader, SectionTally }
+export { HideableTally, Tally, Count, TallyLoader, SectionTally, SectionTallyCount }
